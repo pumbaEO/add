568a13ee02f23a70aa62fb7663cfb6c3dc70bd65
Євген Сосна <shenjasosna@gmail.com>
2018-11-30 13:54:21 +0200

    55

--- tools/linux/build.sh
+++ tools/linux/build.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+set -e
+set -x
+docker ps -a
 CID=$(docker run --detach -e XVFB_RESOLUTION=1920x1080x24 --volume="${PWD}":/home/ubuntu/code onec32/client:latest} client)
 #docker run -d -p 4040:4040 --link "$(cat ./container_id)":http wernight/ngrok ngrok http http:6080 > ./container_idngrok
 docker ps -a && sleep 5
