46e2ad8571ad2429a291c704bea836a1bb3a37b1
Євген Сосна <shenjasosna@gmail.com>
2019-03-13 20:42:58 +0200

    1222

--- tools/linux/release.sh
+++ tools/linux/release.sh
@@ -4,7 +4,7 @@ CID=$(docker run --detach -e XVFB_RESOLUTION=1920x1080x24 --volume="${PWD}":/hom
 #docker run -d -p 4040:4040 --link "$CID":http wernight/ngrok ngrok http http:6080 > ./container_idngrok
 docker ps -a && sleep 5
 #sleep 5 && echo $(curl -s http://127.0.0.1:4040/status | grep -P "http://.*?ngrok.io" -oh)"/vnc_auto.html"
-docker exec -u ubuntu "$CID" /bin/bash -c "cd /home/ubuntu/code; DISPLAY=:1.0 sudo pm build ./"
+docker exec -u ubuntu "$CID" /bin/bash -c "cd /home/ubuntu/code; DISPLAY=:1.0 sudo opm build ./"
 docker exec -u ubuntu "$CID" /bin/bash -c "cd /home/ubuntu/code; DISPLAY=:1.0 sudo chown -R 114 ./"
 docker stop "$CID"
 docker rm "$CID"
